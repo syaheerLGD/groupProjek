60de0e8b7bf24c563bb76bdb8a922a1ae56ffdfb
Muhammad Muammir <72600328+Muammir@users.noreply.github.com>
2021-01-12 19:08:08 +0700

    Add files via upload

--- add register/server1.py
+++ add register/server1.py
@@ -1,1 +1,89 @@
+import os
+import sys
+import threading
+import socket
+import tqdm
+import time
+from cryptography.fernet import Fernet
 
+def encryp(filename,key):
+    f = Fernet(key)
+
+def animation(msg):
+        for char in msg:
+                sys.stdout.write(char)
+                sys.stdout.flush()
+                time.sleep(0.1)
+
+class Server:
+    start = '\t\tWelcome To Secure File Transfer\n'
+    animation(start)
+    print('-------------------------------------------------------------------')
+    numIp = '\tServer IP : 192.168.1.11\n'
+    animation(numIp)
+    def __init__(self):
+        self.sock = socket.socket(socket.AF_INET,socket.SOCK_STREAM)
+        self.Terima_connections()
+    
+    def Terima_connections(self):
+        ip = str(input('\tEnter Ip Address Of Your Server : '))
+        port = int(input('\tEnter Desired Port Number : '))
+
+        self.sock.bind((ip,port))
+        self.sock.listen(100)
+        print('\n')
+
+        while 1:
+            c, addr = self.sock.accept()
+            print('\tConnected to :' + addr[0])
+
+            threading.Thread(target=self.handle_client,args=(c,addr,)).start()
+
+    def handle_client(self,c,addr):
+        with open("login.txt", "w") as register:
+            use = c.recv(1024).decode()
+            pas = c.recv(1024).decode()
+            register.write(use)
+            register.write(":")
+            register.write(pas)
+            c.send("continue".encode())
+
+        with open("login.txt", "r") as login:
+            username = c.recv(1024).decode()
+            password = c.recv(1024).decode()
+
+            jumpa = "tak jumpa";
+            for line in login:
+                creds = line.strip()
+                if creds.split(":")[0] in username and creds.split(":")[1] in password:
+                    jumpa = "jumpa";
+
+            if jumpa == "jumpa":
+                c.send(("\tWelcome back: %s" % (username)).encode())
+            else:
+                c.send("Not-a-user".encode())
+                #c.close()
+
+
+
+        data = c.recv(1024).decode()
+        if not os.path.exists(data):
+            c.send("File Doesn't Exist In The Server".encode())
+
+        else:
+            c.send("File Exist".encode())
+            print('\tSending',data)
+            if data != '':
+                file = open(data,'rb')
+                data = file.read(1024)
+#                encrypted_data = f.encrypt(data)
+                while data:
+                    c.send(data)
+                    data = file.read(1024)
+
+
+                c.shutdown(socket.SHUT_RDWR)
+                c.close()
+                
+
+server = Server()
